60c5a1c12b3de302148704326b62b41ca1baf5c2
Jatinkumar Parmar <62230070+parmarjh@users.noreply.github.com>
2025-09-07 19:50:37 +0530

    Update main.py

--- backend /app /main.py
+++ backend /app /main.py
@@ -1,2 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from app.routes import router
+
+
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+app.include_router(router)
+
+
+if __name__ == '__main__':
+uvicorn.run(app, host='0.0.0.0', port=8000)
